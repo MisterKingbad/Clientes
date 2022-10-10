0b6ddd136d08f241de47d7aff27f810bc8b39db9
MisterKingbad <davymichal20@gmail.com>
2022-10-10 18:01:58 -0300

    feat: :zap: adicionando validadores

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,8 +1,26 @@
+from ast import Str
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validators import *
 
 class ClienteSerializer(serializers.ModelSerializer):
     '''Serializador de clientes'''
     class Meta:
         model = Cliente
         fields = '__all__'
+        
+        
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf':"CPF deve ter 11 dígitos"})
+    
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome':"Nome deve conter apenas letras"})
+        
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg':"RG deve ter 9 dígitos"})
+        
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular':"Celular deve ter no máximo 11 dígitos"})
+        return data
+    
---
+++ clientes/validators.py
@@ -0,0 +1,23 @@
+import re
+from validate_docbr import CPF
+
+
+ 
+def cpf_valido(numero_do_cpf):
+    cpf = CPF()
+    cpf.validate(numero_do_cpf)
+        
+def nome_valido(nome):
+    return nome.isalpha() #se o nome não for alfabético
+        
+        
+def rg_valido(numero_do_rg):
+    return len(numero_do_rg) == 9 #se o tamanho do rg for diferente de 9
+            
+    
+def celular_valido(celular):
+    modelo =  '[0-9]{2} [0-9]{5}-[0-9]{4}' #padrão de celular
+    resposta = re.findall(modelo, celular) #verifica se o celular está no padrão	
+    return resposta
+    
+        
