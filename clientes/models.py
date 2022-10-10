d261fc4306884e4bd173492eab99837babf84bbc
MisterKingbad <davymichal20@gmail.com>
2022-10-10 14:55:07 -0300

    feat: :sparkles: arrumando erros.

--- clientes/models.py
+++ clientes/models.py
@@ -6,8 +6,8 @@ class Cliente(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(blank=false, max_length=30)
     cpf = models.CharField(max_length=11, unique=True)
-    rg = models.CharField(max_jength=9
-    celular = models.CharField(max_length=14)
+    rg = models.CharField(max_jength=9)
+    celula = models.CharField(max_length=14)
     ativo = models.BooleanField()
     
     def __str__(self):
