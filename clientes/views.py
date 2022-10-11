1f20bb37e6895f9b4512ba06b27a585737ce0823
MisterKingbad <davymichal20@gmail.com>
2022-10-11 10:04:22 -0300

    feat: :sparkles: Adicionado pesquisas na api semelhantes ao do admin.
    adicionando campos semelhantes a do django admin no api para facilitar buscas na api.

--- clientes/views.py
+++ clientes/views.py
@@ -1,10 +1,13 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from clientes.serializers import ClienteSerializer
 from clientes.models import Cliente
-
-# Create your views here.
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ClientesViewSet(viewsets.ModelViewSet):
     '''Exibição de clientes'''
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['nome']
+    search_fields = ['nome', 'cpf']   
+    filterset_fields = ['ativo']
